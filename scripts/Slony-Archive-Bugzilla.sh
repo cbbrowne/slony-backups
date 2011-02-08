510857daa824fc510d3fdd985488945e2490d16f
Christopher Browne <cbbrowne@ca.afilias.info>
2011-02-08 11:37:14 -0500

    Add in query to pull closed bugs

--- scripts/Slony-Archive-Bugzilla.sh
+++ scripts/Slony-Archive-Bugzilla.sh
@@ -7,12 +7,15 @@ git fetch origin
 git pull origin master
 wget -O ${BUGHOME}/bugs-not-closed.html "http://www.slony.info/bugzilla/buglist.cgi?query_format=advanced&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED"
 
+wget -O ${BUGHOME}/bugs-closed.html "http://www.slony.info/bugzilla/buglist.cgi?query_format=advanced&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED"
+
 mkdir -p ${BUGHOME}/individual-bugs
 for i in `seq 250`; do
     wget -O ${BUGHOME}/individual-bugs/${i}.html "http://www.slony.info/bugzilla/show_bug.cgi?id=${i}"
 done
 
 git add ${BUGHOME}/bugs-not-closed.html
+git add ${BUGHOME}/bugs-closed.html
 git add ${BUGHOME}/individual-bugs/*.html
 
 git commit -m "Check in bug updates - run of scripts/Slony-Archive-Bugzilla.sh"
