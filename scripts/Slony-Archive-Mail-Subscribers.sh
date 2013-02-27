74e750e1b098e4d3a762378ca6a118303a2a50c0
Christopher Browne <cbbrowne@ca.afilias.info>
2013-02-27 12:57:15 -0500

    Add in script that pulls list of mailing list subscribers, encrypts them using a specified set of GPG keys, and adds them to the backup repository.
    Depends on Python script found at this Git repo:
        https://github.com/cbbrowne/mailman-subscribers

---
+++ scripts/Slony-Archive-Mail-Subscribers.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+ARCHIVEHOME=${HOME}/PostgreSQL/slony-backups/Slony-Mail-Archives
+mkdir -p $ARCHIVEHOME
+cd $ARCHIVEHOME
+git fetch origin
+git pull origin master
+
+# Set up password for Mailman
+MAILMANPASSFILE=${MAILMANPASSFILE:-"${HOME}/GitHome/InitFiles/Slony/slony-mailman-password.sh"}
+### This script sets MAILMANPASSWORD
+. ${MAILMANPASSFILE}
+
+MAILMANSUBTOOL=${MAILMANSUBTOOL:-"${HOME}/GitStuff/mailman-subscribers/mailman-subscribers.py"}
+
+GPGKEYS=${GPGKEYS:-"6AA6A713 5E1E4739"}
+
+for list in bugs commit general hackers patches; do
+    listname=slony1-${list}
+    listfile=${ARCHIVEHOME}/${listname}/subscribers.asc
+    templocation=`mktemp`
+    echo "Grabbing subscribers of Slony list ${listname} into data file: [${templocation}]"
+    python ${MAILMANSUBTOOL} -o ${templocation} lists.slony.info ${listname} "${MAILMANPASSWORD}"
+    echo "Encrypt [${templocation}] into [${listfile}] for GPG user list ${GPGKEYS}"
+    gpg --encrypt --armor -r "${GPGKEYS}" --batch --yes --trust-model always -o ${listfile} ${templocation}
+    rm -f ${templocation}
+    git add ${listfile}
+done
+
+git commit -m "Check in mailing list subscribers - run of scripts/Slony-Archive-Mail-Subscribers.sh"
+git push origin master
