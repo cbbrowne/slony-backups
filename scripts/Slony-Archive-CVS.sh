5bf296d68c8c0449e1519e0f412d298945dd3867
Christopher Browne <cbbrowne@ca.afilias.info>
2013-02-28 11:40:02 -0500

    Add in script that pulls web material from slony.info via CVS

---
+++ scripts/Slony-Archive-CVS.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+ARCHIVEHOME=${HOME}/PostgreSQL/slony-backups
+mkdir -p $ARCHIVEHOME
+cd $ARCHIVEHOME
+git fetch origin
+git pull origin master
+
+if [[ -d ${ARCHIVEHOME}/slony1-www ]]; then
+    echo "CVS Repository already available"
+else
+    echo "No CVS material found at ${ARCHIVEHOME}/slony1-www - setting up anonymous login"
+    CVSROOT=":pserver:anonymous:@main.slony.info:/slony1" cvs login
+fi
+
+git rm -r slony1-www
+CVSROOT=":pserver:anonymous@main.slony.info:/slony1" cvs co slony1-www
+echo "CVS" >> $ARCHIVEHOME/slony1-www/.gitignore
+git add slony1-www
